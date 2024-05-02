f4830e3108958e2a443b8ab4f967095326016ab8
Vineesharajikm <vineesharaj.ikm@kerala.gov.in>
2024-05-02 14:53:24 +0530

    demo

---
+++ src/test/java/com/example/demogitignore/DemogitignoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogitignore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitignoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
